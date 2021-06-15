9adf029ebf7f9b0df62e9448abdc07c0472f0121
pedro392 <souger1948@hotmail.co.jp>
2021-06-15 23:01:56 +0900

    コミットメッセージ

--- task.rb
+++ task.rb
@@ -31,7 +31,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  newary = sports.compact!
+  sports.compact!
 
   # 以下は変更しないで下さい
   p sports
@@ -51,6 +51,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  numbers2 = numbers1.map { |n| n * 10}
+  p numbers2
 
 end
 
@@ -58,7 +60,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -67,6 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -77,13 +81,22 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    p "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
+  foods.each do |food|
+    if food.include? ("うに")
+      p "#{food}は好物です"
+    else
+      p "#{food}はまぁまぁ好きです"
+    end
+  end
 
 end
 
